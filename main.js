c6b3960d4b8a4937c25974315e882250efa565e2
mblester <morganmurry@gmail.com>
2022-05-19 20:43:27 -0500

    3 working tests

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert');
 const readline = require('readline');
+const { isRegExp } = require('util/types');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -23,6 +24,7 @@ const countIt = (str) => {
     }
   }
   console.log(sortObj)
+  return sortObj
 }
 
 
@@ -40,10 +42,24 @@ if (typeof describe === 'function') {
       assert.equal(typeof countIt, "function");
     });
   });
+  describe('Should take a string', ()=>{
+    it('should accept a string', ()=>{
+      
+      assert.equal(typeof 'hello', 'string')
+    })
+    it('should do something with string', ()=>{
+      
+      assert.equal(typeof str, 'string')
+    })
+  })
+  describe('Count it function breaks down the string', ()=>{
+    it('Should return an object with letters as the key and the values as the amount the letter appears', ()=>{
+      assert.equal(String(countIt('Hello')), String({ h: 1, e: 1, l: 2, o: 1}))
+    })
+  })
 } else {
   getPrompt();
 }
 
 
 
-// let newstr = str.replace( /[^A-Za-z0-9]/g, '')

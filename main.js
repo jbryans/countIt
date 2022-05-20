e7dc2a88dc751404177701fbf77aeaefe444aca1
Jake Bryans <97806024+jbryans@users.noreply.github.com>
2022-05-19 20:39:35 -0500

    Revert "jakes changes "

--- main.js
+++ main.js
@@ -9,35 +9,10 @@ const rl = readline.createInterface({
 
 
 
-
-console.clear()
 const countIt = (str) => {
-  let cleanString = str.toLowerCase().replace(/[^A-Za-z0-9]/g, '')
-  console.log(cleanString)
-  const counts = {};
-  for (const i of cleanString) {
-    if (counts[i]) {
-      counts[i]++
-    } else {
-      counts[i] = 1
-    }
-  }
-  return counts;
+  console.log('BOOMS', str.length)
 }
 
-// const string = "I want to count the number of occurences of each char in this string";
-// console.log(countChar(string))
-
-// const obj = {
-//   name: 1,
-//   address: "San Marcos"
-// }
-
-// console.log(obj)
-
-// obj.name = 'string'
-// obj.address = 1
-// obj.address++
 
 const getPrompt = () =>  {
   rl.question('Enter your string to count the characters: ', (str) => {
@@ -45,20 +20,6 @@ const getPrompt = () =>  {
     getPrompt();
   });
 }
-
-//take any string 
-
-//remove whitespace/convert to lower case
-//toLowerCase()/.trim()
-//.split to show each letter in  an array indiividually
-//the output shows the letters individually and how often they appear in the string
-
-
-//**********************************
-//**********************************
-
-
-
 
 if (typeof describe === 'function') {
   describe('#countIt()', () => {
